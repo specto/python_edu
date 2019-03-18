52e678d524a0c48398812f10f22e38f964cdd9bf
Velichko Karakostov <vivo@specto.org>
2019-03-18 20:32:00 +0200

    Find repeated ids

--- parser.py
+++ parser.py
@@ -1,26 +1,34 @@
-from datetime import datetime
+import re
 
-records = [{
-    'author': 'Ilia',
-    'date_created': datetime.now(),
-    'title': 'Magnum opus',
-    'description': 'bla bla',
-    'pages': [
-        'Lorem ipsum',
-        'asdfasdf asdf asdf asfd',
-        'asdf asdfa sdfa sdf',
-    ],
-}, {
-    'author': 'Boyan',
-    'date_created': datetime.now(),
-    'title': 'Diplomna rabota',
-    'description': 'bla bla',
-    'pages': [
-        'Shellcode',
-        'bla bla shellcode bla bla',
-        'asdf asdfa sdfa sdf',
-    ],
-}]
+ids = []
+end_braces = []
 
-for record in records:
-    print(record['title'])
+
+entry_re = re.compile(r'@\w+{(.+),\n')
+
+with open("library.bib", "r") as library_file:
+    brace_count = 0
+    in_brace = False
+    for line in library_file:
+        for char in line:
+            if char == '{':
+                brace_count += 1
+            elif char == '}':
+                brace_count -= 1
+            if not in_brace and brace_count == 1:
+                ids.append(entry_re.sub(lambda m: m.group(1), line))
+                in_brace = True
+            if in_brace and brace_count == 0:
+                end_braces.append('-')
+                in_brace = False
+
+print(len(ids) - len(set(ids)))
+
+id_counts = {}
+for name in ids:
+    id_counts[name] = id_counts.get(name, 0) + 1
+
+for name, count in id_counts.items():
+    if count > 1:
+        print(name, count)
+# print(brace_count)
